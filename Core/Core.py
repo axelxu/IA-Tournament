ba0073aef7dcff48f2d3dd5db4b462ccff8c5523
axel xu <axel.xu25@gmail.com>
2018-11-19 11:35:08 +0100

    début

---
+++ Core/Core.py
@@ -0,0 +1,5 @@
+class Case:#on crée la classe case qu'on ajoutera dans nos matrices
+    def __init__(self,coordonees,vide,etat):
+        self.coordonees = coordonees
+        self.vide = vide
+        self.etat = etat
