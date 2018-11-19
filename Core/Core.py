d20853e9589fa518b13dc0b1194aec34750848b0
axel xu <axel.xu25@gmail.com>
2018-11-19 14:11:30 +0100

    création de la classe Case et plateau

--- Core/Core.py
+++ Core/Core.py
@@ -5,11 +5,12 @@ class Case:#on crée la classe case qu'on ajoutera dans nos matrices
         self.etat = etat
 
 class plateau : #on crée la matric des cases
-    def __init__(self,taille):
-        plateau.taille=taille
-        plateau.surface=[[Case((i,j),True,0) for j in range(taille)] for i in range(taille)]
+    def __init__(self,Jeu):
+        plateau.Jeu=Jeu
+        plateau.surface=[[Case((i,j),True,0) for j in range(Jeu.largeur)] for i in range(Jeu.hauteur)]
 
     def get_valeur(self,i,j): #chope la valeur de tel case dans le plateau
         return(plateau.surface[i][j])
     def set_case(self,i,j,vide,etat):
         plateau.surface[i][j] = Case((i,j),vide,etat)
+
